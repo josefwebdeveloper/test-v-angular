f1ce282d466bb20c3867debb7efaf77fdd472424
Josef Shainskiy <josef2011@gmail.com>
2022-02-13 18:00:12 +0200

    first

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,62 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {
+  //   path: '',
+  //   component: AppComponent,
+  // },
+  // {
+  //   path: 'welcome',
+  //   component: WelcomeComponent,
+  // },
+  //
+  // {
+  //   path: 'patient-list',
+  //   component: PatientListComponent,
+  //   canActivate: [AuthGuardService],
+  //   resolve: { programResolver: ProgramResolver },
+  // },
+  //
+  // {
+  //   path: 'teammates',
+  //   component: TeammatesComponent,
+  //   canActivate: [AuthGuardService],
+  //   resolve: {
+  //     programResolver: ProgramResolver,
+  //   },
+  //   children: [
+  //     { path: '', redirectTo: 'list', pathMatch: 'full' },
+  //     { path: 'list', component: TeammateListComponent },
+  //     { path: 'invitations', component: PendingInvitationComponent },
+  //   ],
+  // },
+  //
+  // {
+  //   path: 'patient-detail/:id',
+  //   component: PatientDetailComponent,
+  //   canActivate: [AuthGuardService],
+  //   resolve: { programResolver: ProgramResolver },
+  // },
+  // {
+  //   path: 'program-settings',
+  //   component: ProgramSettingsComponent,
+  //   canActivate: [AuthGuardService],
+  //   resolve: { programResolver: ProgramResolver },
+  // },
+  //
+  //
+  //
+  //
+  // {
+  //   path: '404',
+  //   component: PageNotFoundComponent,
+  // },
+  // {
+  //   path: '**',
+  //   redirectTo: '/404',
+  // },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
